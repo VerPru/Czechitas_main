098ba7fdbb9c008fdf951771bd05bac798c66e37
Veronika <veronika.pruchova@intelstudios.com>
2024-10-28 09:53:30 +0100

    změny 1

--- Czechitas_lekce05/script05.js
+++ Czechitas_lekce05/script05.js
@@ -6,3 +6,10 @@ nadpis.style.color="yellow"
 nadpis.style.backgroundColor="red"
 
 console.log(nadpis2)
+
+nadpis.classList.add("ramecek") //pridam ten elememt a definuju si ho v ccs souboru, je to case sensitive
+
+//hastag níže píšu, proto že když zakladam id tak píšu #něco
+document.querySelector("#obrazek-kocka").src="https://media.istockphoto.com/id/1443562748/photo/cute-ginger-cat.jpg?s=612x612&w=0&k=20&c=vvM97wWz-hMj7DLzfpYRmY2VswTqcFEKkC437hxm3Cg="
+document.querySelector("#obrazek-kocka").style.width = "200px"
+document.querySelector("#obrazek-kocka").alt = "Krásná kočička" //alternativní text
