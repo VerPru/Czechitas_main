5cd42681ad5d99e7d8fe5bab7aea0040adc1757c
Veronika <veronika.pruchova@intelstudios.com>
2024-10-28 20:21:45 +0100

    final

--- Czechites_lekce06/script06.js
+++ Czechites_lekce06/script06.js
@@ -38,7 +38,7 @@ console.log(cas2.isAfter(novyRok))
 */
 
 
-//UKOL-1 Cvičení: Knihovny
+/*UKOL-1 Cvičení: Knihovny
 
 const email = prompt("Prosím tě člověče, zadej e-mail");
 const msgElement = document.getElementById("msg");
@@ -55,3 +55,139 @@ if (validator.isEmail(email)) {
 
 console.log(validator.isEmail("ja@seznam.cz"));
 console.log(validator.isCreditCard("4125010001000208"));
+
+*/
+
+/*PO PŘESTÁVCE - JEDNA
+
+tohle se jmenuje arrow funkce, pokazde kdyz se zavolat ta funkce ahoj, pokazde se napíše i to "Nečum" mohu do něj doplnovat informace, jsou ruzne druhy jak dostat tu informaci - vypisovat, pak matematicke operace, pak i s podminkou
+
+const ahoj = (jmeno, idZamestnance) => {
+    console.log(`Nečum na mě ${jmeno} - ${idZamestnance}`)
+}
+
+
+ahoj("Michale", 123)
+ahoj("Jano", 12)
+ahoj("Eriku")
+ahoj("Lucie", 1234)
+
+
+const nasobeni = (cislo1, cislo2) => {
+    return cislo1 * cislo2
+}
+console.log(nasobeni(10, 10))
+const spravneTelCislo = (tel) => {
+    if(validator.isMobilePhone(tel, "cs-CZ")) {
+        return true
+    } else {
+        return false
+    }
+}
+console.log(spravneTelCislo("123456789"))
+*/
+
+/*
+//DALŠÍ PŘÍKLAD OD MICHALA - tady si definuju tu funkci, takže když se něco změní, tak to měním jen na tomhle místě - třeba když bude muset mít 16míst místo 12ti míst :) Nemusím to měnit na dalších tisících místech jen tady prostě
+
+const bezpecneHeslo =(heslo)=> {
+    if(heslo.lenght > 12 && heslo.contains("@")){
+        return true
+    }
+
+    return false
+}
+
+const heslo = prompt ("Zadej heslo")
+
+if(bezpecneHeslo(heslo)) {
+    console.log("Super!")
+} else {
+    console.log("Ne-bezpečné heslo")
+}
+*/
+
+// ÚKOL - Obsah elipsy
+
+document.body.innerHTML += '<h1>Obsah elipsy</h1>'
+
+const ellipseArea = (vyska, sirka) => {
+    return (vyska / 2) * (sirka / 2) * Math.PI;
+}
+
+// Funkce pro zobrazení výsledků na stránce
+const displayEllipseArea = (vyska, sirka) => {
+    const area = ellipseArea(vyska, sirka);
+    document.body.innerHTML += `<p>Obsah elipsy pro šířku ${vyska} a výšku ${sirka} je: ${area}</p>`;
+}
+
+// Testování funkce s různými hodnotami a zobrazení výsledků na stránce
+displayEllipseArea(1, 2);  // Očekávaný výsledek: 1.5707963267948966
+displayEllipseArea(2, 2);  // Očekávaný výsledek: 3.141592653589793
+displayEllipseArea(3, 5);  // Další testovací hodnota
+
+//ÚKOL - MAX ZE DVOU ČÍSEL
+
+document.body.innerHTML += '<h1>Max ze dvou čísel</h1>'
+
+const max2 = (num1, num2) => {
+    if (num1 > num2) {
+        return num1;
+    } else {
+        return num2;
+    }
+}
+
+// Funkce pro zobrazení výsledků na stránce
+const displayMax2Result = (num1, num2) => {
+    const maxNumber = max2(num1, num2);
+    document.body.innerHTML += `<p>Větší číslo z ${num1} a ${num2} je: ${maxNumber}</p>`;
+}
+
+// Testování funkce s různými hodnotami a zobrazení výsledků na stránce
+displayMax2Result(10, 20);   // Očekávaný výsledek: 20
+displayMax2Result(30, 25);   // Očekávaný výsledek: 30
+displayMax2Result(7, 7);     // Očekávaný výsledek: 7 (v případě shody vrátí jedno z nich)
+
+//ÚKOL - Kontrola DIČ
+
+document.body.innerHTML += '<h1>Kontrola DIČ - v konzoli</h1>'
+
+const isDIC = (inputStr) => {
+    // Zkontrolujeme, zda je řetězec kratší než 11 znaků
+    if (inputStr.length < 11) {
+        return false;
+    }
+
+    // Zkontrolujeme, zda je řetězec delší než 12 znaků
+    if (inputStr.length > 12) {
+        return false;
+    }
+
+    // Uložíme první dva znaky do proměnné prefix
+    const prefix = inputStr.slice(0, 2);
+
+    // Zkontrolujeme, jestli prefix je "CZ"
+    if (prefix !== "CZ") {
+        return false;
+    }
+
+    // Uložíme zbytek řetězce (od třetího znaku dál) do proměnné digits
+    const digits = inputStr.slice(2);
+
+    // Zkontrolujeme, zda jsou všechny znaky v proměnné digits číslice
+    if (!validator.isInt(digits)) {
+        return false;
+    }
+
+    // Pokud funkce došla až sem, všechny testy prošly, vrátíme true
+    return true;
+};
+
+// Testování funkce s různými vstupy
+console.log(isDIC("CZ123456789"));    // Očekávaný výsledek: true (platné DIČ)
+console.log(isDIC("CZ12345678"));     // Očekávaný výsledek: false (méně než 11 znaků)
+console.log(isDIC("CZ1234567890"));  // Očekávaný výsledek: true (platné DIČ)
+console.log(isDIC("CZ123456789012")); // Očekávaný výsledek: false (více než 12 znaků)
+console.log(isDIC("SK123456789"));    // Očekávaný výsledek: false (neplatný prefix)
+console.log(isDIC("CZ12345abc"));     // Očekávaný výsledek: false (obsahuje nečíselné znaky)
