9b83c331bbe88b567bb6a6cc724d905a22b37d0e
Veronika <veronika.pruchova@intelstudios.com>
2024-10-28 19:15:44 +0100

    Vánoce

---
+++ Czechites_lekce06/Vanoce.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const messageElement = document.getElementById("message");
+    const today = new Date();
+    const startOfChristmasSeason = new Date(today.getFullYear(), 11, 1); // 1. prosince
+    const endOfChristmasSeason = new Date(today.getFullYear(), 11, 31); // 31. prosince
+
+    if (today >= startOfChristmasSeason && today <= endOfChristmasSeason) {
+        document.body.classList.add("christmas-background");
+        messageElement.textContent = "Ano, už je vánoční čas!";
+    } else {
+        document.body.classList.add("neutral-background");
+        messageElement.textContent = "Ne, ještě není vánoční čas.";
+    }
+});
